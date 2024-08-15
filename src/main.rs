5d2f4ba2986e3f00572070fe15dd07f2b9778d6f
notmario <59957544+notmario@users.noreply.github.com>
2024-08-15 18:54:41 +0800

    0.1

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,22 @@ fn main() {
     println!("clearing out {}", OUT_DIR);
     let _ = fs::remove_dir_all(OUT_DIR);
     let _ = fs::create_dir(OUT_DIR);
+    let _ = fs::write(format!("{}/.nojekyll", OUT_DIR), "");
     let mut backlinks: HashMap<String, Vec<String>> = HashMap::new();
     let mut files: HashMap<String, String> = HashMap::new();
+
+    let template = fs::read_to_string("template.html").expect("should exist");
 
     // find backlinks
     for entry in WalkDir::new(IN_DIR).into_iter().filter_map(|e| e.ok()) {
         let real_path = format!("{}", entry.path().display());
-        let real_path = real_path.strip_prefix(IN_DIR).expect("balls");
-        println!("finding backlinks in {}", real_path);
+        let real_path = real_path
+            .strip_prefix(IN_DIR)
+            .expect("if it doesn't have it something has GONE WRONG");
+        println!("found {}", real_path);
         if entry.path().is_dir() {
             let _ = fs::create_dir(format!("{}{}", OUT_DIR, real_path));
-        } else {
+        } else if real_path.ends_with(".pssg") {
             let f = fs::read_to_string(entry.path())
                 .unwrap_or("this is an absolutely FUCKED UP file".into())
                 .replace("\r\n", "\n");
@@ -31,29 +36,37 @@ fn main() {
                     this_backlinks.push(real_path.to_string().replace("\\", "/"));
                     println!(
                         "backlink of type {}! taking note",
-                        l.strip_suffix(" <--").expect("ski")
+                        l.strip_suffix(" <--").expect("ends with")
                     );
                 }
             }
             files.insert(real_path.to_string().replace("\\", "/"), f);
         }
     }
+    for (_, l) in backlinks.iter_mut() {
+        l.sort()
+    }
     // insert backlinks
     let files: HashMap<String, String> = files
         .iter()
         .map(|(k, f)| {
             let mut new_content = String::new();
+            if k != "index.pssg" {
+                new_content.push_str("=> index.pssg\n\n");
+            }
             for l in f.lines() {
                 new_content.push_str(l);
                 new_content.push('\n');
                 if l.starts_with("--> ") {
                     let backlink_type = l.strip_prefix("--> ").expect("starts with it");
-                    let my_backlinks = backlinks.get(backlink_type).expect("shitball");
-                    for l in my_backlinks {
-                        println!("{} now links to {}", k, l);
-                        new_content.push_str("=> ");
-                        new_content.push_str(l);
-                        new_content.push('\n');
+                    let my_backlinks = backlinks.get(backlink_type);
+                    if my_backlinks.is_some() {
+                        for l in my_backlinks.unwrap() {
+                            println!("{} now links to {}", k, l);
+                            new_content.push_str("=> ");
+                            new_content.push_str(l);
+                            new_content.push('\n');
+                        }
                     }
                 }
             }
@@ -69,6 +82,11 @@ fn main() {
             for l in f.lines() {
                 if l.starts_with("=> ") {
                     let url = l.strip_prefix("=> ").expect("starts with");
+                    let url = if url.contains("|") {
+                        url.split("|").last().expect("it should exist")
+                    } else {
+                        url
+                    };
                     if url.starts_with("http") {
                         new_content.push_str("<a href=\"");
                         new_content.push_str(url);
@@ -87,8 +105,36 @@ fn main() {
                                 break;
                             }
                         }
-                        println!("shared prefix: {}", common);
+                        let get_back_to_there = k
+                            .strip_prefix(&common)
+                            .expect("we know it starts with it")
+                            .split("/")
+                            .skip(1)
+                            .map(|a| "../")
+                            .collect::<Box<[&str]>>()
+                            .join("");
+                        let then_go = url
+                            .strip_prefix(&common)
+                            .expect("we know it starts with it");
+
+                        let then_go = if then_go.ends_with(".pssg") {
+                            format!("{}.html", then_go.strip_suffix(".pssg").expect("ends with"))
+                        } else {
+                            then_go.into()
+                        };
+
+                        let relative_url = format!("{}{}", get_back_to_there, then_go);
+
+                        new_content.push_str("<a href=\"");
+                        new_content.push_str(&relative_url);
+                        new_content.push_str("\">");
+                        new_content.push_str(l);
+                        new_content.push_str("</a>\n");
                     }
+                } else if l.starts_with("#") {
+                    new_content.push_str("<b>");
+                    new_content.push_str(l);
+                    new_content.push_str("</b>\n")
                 } else {
                     new_content.push_str(l);
                     new_content.push('\n');
@@ -100,5 +146,11 @@ fn main() {
 
     for (k, v) in files {
         println!("===\n{}\n{}", k, v);
+        let out_thing = format!(
+            "{}{}.html",
+            OUT_DIR,
+            k.strip_suffix(".pssg").expect("um i dunno")
+        );
+        let _ = fs::write(out_thing, template.replace("||content||", &v));
     }
 }
